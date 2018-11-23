a740de57154bf60d74dd36dd205fa666a617d6cb
Damir Zekić <damirz@gmail.com>
2018-11-23 11:38:48 +0100

    Initial commit
    Generate the gem boilerplate with `bundle gem` command.

---
+++ lib/omniauth/samedi.rb
@@ -0,0 +1,8 @@
+require "omniauth/samedi/version"
+
+module Omniauth
+  module Samedi
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/omniauth/samedi_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Omniauth::Samedi do
+  it "has a version number" do
+    expect(Omniauth::Samedi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
